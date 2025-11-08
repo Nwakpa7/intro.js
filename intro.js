2c69fd58ec90b4667fba2ec7a75fc88e8f0d8646
Nwakpa7 <nwakpaelijah7@gmail.com>
2025-11-08 17:19:16 +0100

    name, age and hobbies.

---
+++ intro.js
@@ -0,0 +1,13 @@
+const user = { 
+    name: "Elijah",
+     Age: 23,
+     lang: "C" };
+
+
+     console.log( user.name +  " is "  + user.Age  +  " years old and my favourite programming language is " +  user.lang );
+     
+
+
+
+
+
